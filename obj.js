886c1311c6e91cb6d1e820b2ed3c39f4ca70b32a
abisha-2205 <abishaabi13661@gmail.com>
2024-08-30 16:39:58 +0530

    Add files via upload

---
+++ obj.js
@@ -0,0 +1,11 @@
+const mobile={
+    brand:"oppo",
+    model:"A55",
+    year:2020,
+    start:function(){
+        console.log("The mobile is activated");
+    }
+};
+console.log(mobile.model);
+console.log(mobile.year);
+mobile.start();
